5466102f162b303d4d0b667e966e515c8e3390fd
jellyfish26 <jellyfish0162@gmail.com>
2020-11-01 17:47:55 +0900

    add revesi and main site

--- site.tf
+++ site.tf
@@ -3,3 +3,15 @@ module "clicker-site" {
   name = "clicker"
   domain = aws_route53_zone.fes_main
 }
+
+module "reversi-site" {
+  source = "./module/cloudfront"
+  name = "reversi"
+  domain = aws_route53_zone.fes_main
+}
+
+module "main-site" {
+  source = "./module/cloudfront"
+  name = "site"
+  domain = aws_route53_zone.fes_main
+}
